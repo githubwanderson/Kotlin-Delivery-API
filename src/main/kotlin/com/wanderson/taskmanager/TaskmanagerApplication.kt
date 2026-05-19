fa4739605ee5e26818d77cfd32ad1fab428dc165
Wanderson <wanderson.martins@magis5.com.br>
2026-05-19 11:22:12 -0300

    Criado projeto

---
+++ src/main/kotlin/com/wanderson/taskmanager/TaskmanagerApplication.kt
@@ -0,0 +1,11 @@
+package com.wanderson.taskmanager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TaskmanagerApplication
+
+fun main(args: Array<String>) {
+	runApplication<TaskmanagerApplication>(*args)
+}
